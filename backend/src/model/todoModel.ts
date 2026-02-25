d96ffdd2eb7b1753bca16bf1ef29fce33218675f
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2026-02-25 21:23:49 +0530

    feat(todoModel) :todomodel created

---
+++ backend/src/model/todoModel.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    status: {
+      type: String,
+      enum: ["Pending", "In-Progress", "Completed"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Todo", todoSchema)
